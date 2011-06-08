070c4185f56ade7627f64d6939713c4b61535043
Jake Gordon <jake@codeincomplete.com>
2011-06-08 07:43:43 -0700

    pass the canvas element as 2nd parameter to requestAnimationFrame.
    From Mozilla:
    
      "A optional parameter (not used by Firefox) specifying the element that visually bounds the entire animation. For canvas and WebGL, this should be the actual <canvas> element. For other DOM elements, you can leave it off or define it for a slightly more optimized experience"

--- stutter.js
+++ stutter.js
@@ -45,9 +45,9 @@ Stutter = function(id) {
     update(now - last);
     render();
     last = now;
-    requestAnimationFrame(frame);
+    requestAnimationFrame(frame, canvas);
   };
-  requestAnimationFrame(frame);
+  requestAnimationFrame(frame, canvas);
 
 //=================
 // USE setInterval:
